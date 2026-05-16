86663f9c831dc781e46be163000302a2606fd864
ARYAN VISHWAKARMA <148914861+ArYaN14138@users.noreply.github.com>
2026-05-16 23:44:18 +0530

    Time: 35 ms (43.1%), Space: 59.3 MB (32.56%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,82 @@
+class Solution {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+
+                    // skip duplicates
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+
+                } 
+                else if (sum < 0) {
+                    left++;
+                } 
+                else {
+                    right--;
+                }
+            }
+        }
+        return res;
+    }
+}
+/*
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+ List<List<Integer>> ans = new ArrayList<>();
+ Arrays.sort(nums);
+
+        int n =nums.length;
+
+        for(int i=0;i<n-2;i++)
+        {
+
+            if(i>0 && nums[i]==nums[i-1])  continue;
+
+            for(int j=i+1;j<n-1;j++)
+            {
+               if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                for(int k=j+1;k<n;k++)
+                {
+                    if(k>j+1 && nums[k]==nums[k-1]) continue;
+
+                    if(nums[i]+nums[j]+nums[k]==0)
+                    {
+                        ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
+
+                    }
+
+
+                }
+            }
+        }
+
+        return ans;
+
+
+        
+    }
+}
+*/
